45322d6d56b01debbffaeae4b978c0a26ec7f1d8
Elmavis <37665679+Elmavis@users.noreply.github.com>
2018-09-20 15:30:35 +0300

    JSlove

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for(var i = 0; i < preferences.length; i++)
+  {
+    if(preferences[preferences[preferences[i] - 1] - 1] - 1 == i)
+      count++;
+  }
+  return Math.floor(count/3);
 };
